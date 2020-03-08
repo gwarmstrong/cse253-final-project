b6cb5419efa719da357c10581221f53ad40100b7
garmstro <garmstro@garmstro-28577.pods.garmstro.svc.cluster.local>
2020-03-08 12:36:38 -0700

    logging functionality via CLI

--- colorme/scripts/training.py
+++ colorme/scripts/training.py
@@ -1,6 +1,18 @@
+import sys
 import click
+import logging
 from colorme.training import train_baseline, train_baseline_gan
 
+
+log_levels = {
+    'info' : logging.INFO,
+    'INFO' : logging.INFO,
+    'debug' : logging.DEBUG,
+    'DEBUG' : logging.DEBUG,
+    'WARNING' : logging.WARNING,
+    'ERROR' : logging.ERROR,
+    'CRITICAL' : logging.CRITICAL,
+}
 
 @click.group()
 def colorme():
@@ -14,11 +26,19 @@ def train():
 
 @train.command()
 @click.option('--config', type=click.Path(exists=True))
-def baseline(config):
+@click.option('--log', default=None)
+@click.option('--logstream', default=sys.stdout)
+def baseline(config, log, logstream):
+    level = log_levels.get(log, logging.ERROR)
+    logging.basicConfig(stream=logstream, level=level)
     train_baseline(config_path=config)
 
 
 @train.command()
 @click.option('--config', type=click.Path(exists=True))
-def baseline_gan(config):
+@click.option('--log', default=None)
+@click.option('--logstream', default=sys.stdout)
+def baseline_gan(config, log, logstream):
+    level = log_levels.get(log, logging.ERROR)
+    logging.basicConfig(stream=logstream, level=level)
     train_baseline_gan(config_path=config)
