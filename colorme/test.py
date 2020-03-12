e7cf2f841eb091b9eabd759108bf267a1a6a37b6
George Armstrong <garmstrong@colgate.edu>
2020-03-12 11:40:20 -0700

    revert accidental all to lower...

--- colorme/test.py
+++ colorme/test.py
@@ -22,7 +22,7 @@ def load_model(model_path, use_gpu):
 
     model_args.pop("self", None)  # TODO: WHAT I DONT EVEN
     model_args.pop("__class__", None)  # TODO: ARE YOU SERIOUS RIGHT NOW?
-    model_args["use_gpu"] = use_gpu  # todo: i swear...
+    model_args["use_gpu"] = use_gpu  # TODO: I swear...
     print("-----")
     print(model_args)
 
@@ -30,48 +30,48 @@ def load_model(model_path, use_gpu):
                                      **model_args)
     model.load_state_dict(checkpoint['state_dict'])
 
-    model.use_gpu = use_gpu    # todo this feels not particularly robust
+    model.use_gpu = use_gpu    # TODO This feels not particularly robust
     if use_gpu:
         model = model.cuda()
 
-    # todo:  checkpoints have a gstate_dict and a dstate_dict and a goptimizer
-    #  and a doptimizer, do i need to deal with this?
+    # TODO:  checkpoints have a Gstate_dict and a Dstate_dict and a Goptimizer
+    #  and a Doptimizer, do I need to deal with this?
     return model
 
 
 def eval_test(config_path, model_path):
     config = load_config(config_path)
 
-    # todo these should be sorted in the order they're called
+    # TODO these should be sorted in the order they're called
     test_data = config["test_data"]
-    random_seed = config.get("random_seed", none)
+    random_seed = config.get("random_seed", None)
     batch_size = config.get("batch_size", 1)
-    eval_mode = config.get("eval_mode", true)
+    eval_mode = config.get("eval_mode", True)
     use_gpu = config.get("use_gpu", torch.cuda.is_available())
 
     # 0 is default dataloader value for num_workers
     num_workers = config.get("num_workers", 0)
 
-    # # todo may want to figure out a way to make this more general
+    # # TODO may want to figure out a way to make this more general
     # input_dimensions = (1, 1, image_size, image_size)
-    # generator_kwargs = {'inputdimensions': input_dimensions}
+    # generator_kwargs = {'inputDimensions': input_dimensions}
     # logdir = config.get("logdir", os.path.join(os.curdir, 'logs'))
 
-    if random_seed is not none:
+    if random_seed is not None:
         torch.manual_seed(random_seed)
 
-    test_dataset = imagedataset(path_file=test_data, random_seed=random_seed)
-    test_dataloader = dataloader(
+    test_dataset = ImageDataset(path_file=test_data, random_seed=random_seed)
+    test_dataloader = DataLoader(
         test_dataset,
         batch_size=batch_size,
-        shuffle=true,
+        shuffle=True,
         num_workers=num_workers,
     )
 
     model = load_model(model_path, use_gpu)
 
     if eval_mode:
-        # todo: this switches mode of dropout and batchnorm, but
+        # TODO: This switches mode of dropout and batchnorm, but
         #  we might also want to swap batchnorm for instance norm?
         model = model.eval()
 
@@ -81,27 +81,27 @@ def eval_test(config_path, model_path):
     total_ssim_loss = 0
     total_g_loss = 0
 
-    ssim = ssim_loss()
-    for i, (x_gray, x_color) in enumerate(test_dataloader):
-        fake_label = torch.full((x_color.size(0),), model.fake_label)
-        real_label = torch.full((x_color.size(0),), model.real_label)
+    ssim = SSIM_Loss()
+    for i, (X_gray, X_color) in enumerate(test_dataloader):
+        fake_label = torch.full((X_color.size(0),), model.fake_label)
+        real_label = torch.full((X_color.size(0),), model.real_label)
         if use_gpu:
             real_label = real_label.cuda()
             fake_label = fake_label.cuda()
-            x_gray = x_gray.cuda()
-            x_color = x_color.cuda()
+            X_gray = X_gray.cuda()
+            X_color = X_color.cuda()
 
-        x_fake, disc_fake = model.forward(x_gray, train='none', skip_generator=false)
-        x_color, disc_real = model.forward(x_color, train='none', skip_generator=true)
+        X_fake, disc_fake = model.forward(X_gray, train='none', skip_generator=False)
+        X_color, disc_real = model.forward(X_color, train='none', skip_generator=True)
 
-        g_loss = model.gcriterion(x_fake, x_color)
-        ssim_loss = ssim.forward(x_fake, x_color)
+        g_loss = model.Gcriterion(X_fake, X_color)
+        ssim_loss = ssim.forward(X_fake, X_color)
 
-        # ehh, dcriterion isn't super informative
-        # d_loss_real = model.dcriterion(disc_fake, fake_label)
-        # d_loss_fake = model.dcriterion(disc_real, real_label)
+        # Ehh, Dcriterion isn't super informative
+        # d_loss_real = model.Dcriterion(disc_fake, fake_label)
+        # d_loss_fake = model.Dcriterion(disc_real, real_label)
 
-        total_processed += x_fake.shape[0]
+        total_processed += X_fake.shape[0]
         total_disc_real += disc_real.sum()
         total_disc_fake += disc_fake.sum()
         total_ssim_loss += ssim_loss
